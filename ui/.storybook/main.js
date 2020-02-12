e93be47e0f5adcac679eae1ac5ffc27ab2ad3a51
codebelt <code@codebelt.com>
2020-02-11 19:59:28 -0600

    add storybook

---
+++ ui/.storybook/main.js
@@ -0,0 +1,4 @@
+module.exports = {
+  stories: ['../src/**/*.stories.(jsx|mdx)'],
+  addons: ['@storybook/addon-actions/register', '@storybook/addon-links/register'],
+};
---
+++ ui/.storybook/webpack.config.js
@@ -0,0 +1,8 @@
+module.exports = ({ config, mode }) => {
+
+  // config.resolve.alias['environment'] = path.join(__dirname, '../src/environments', process.env.CLIENT_ENV || 'development');
+
+  // config.resolve.extensions.push('.ts', '.tsx');
+
+  return config;
+};
